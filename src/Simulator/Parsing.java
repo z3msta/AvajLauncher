e116ad9be82c5c95e97ad34e690f79230faa39b9
z3msta <mehdifaiz42@outlook.com>
2025-03-11 21:00:15 +0100

    working on parsing

--- src/Simulator/Parsing.java
+++ src/Simulator/Parsing.java
@@ -2,10 +2,26 @@ package Simulator;
 
 import java.util.List;
 
+import static java.lang.System.exit;
+
+
 public class Parsing {
 
     public int fileParsing(List<String> data) {
 
+        int i = 1;
+
+        try {
+            int number = Integer.parseInt(data.get(0));
+        } catch (NumberFormatException e) {
+            return -1;
+        }
+
+        while (data.size() > i) {
+
+            System.out.println(data.get(i));
+            i++;
+        }
         return -1;
     }
 }
